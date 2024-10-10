c5b2b3de52e8456a30de3908e3d09ed8b1a8963f
Edgar <edgargarteong@gmail.com>
2024-10-11 03:23:39 +0800

    finished typescript section

--- part9_typescript_fso/exercises/patientor/backend/src/data/patients.ts
+++ part9_typescript_fso/exercises/patientor/backend/src/data/patients.ts
@@ -47,6 +47,7 @@ const data = [
 const patients: Patient[] = data.map(obj => {
     const object = toNewPatient(obj) as Patient; 
     object.id = obj.id; 
+    object.entries = [];
     return object;
 }); 
 
